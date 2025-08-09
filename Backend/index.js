4543e240f2f3cdb4b2ebaf428ecf0326e3e7c74e
abhay <deepatiwari048@gmial.com>
2025-08-09 14:58:13 +0530

    checkBackend

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ app.use(cors({
  origin:["http://localhost:5173" , "http://localhost:5174"],
  credentials:true
 }))
-
+app.get("/", (req, res) => res.send("Backend is running"));
 app.use("/api/auth",authRoutes)
 app.use("/api/user",userRoutes)
 app.use("/api/product",productRoutes)
