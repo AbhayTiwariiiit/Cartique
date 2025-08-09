a2ac4f61bdd2324adc89cd0702622cb32c0e5b3b
Abhay <118330713+AbhayTiwariiiit@users.noreply.github.com>
2025-08-09 15:53:09 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["https://cartiquefrontend.onrender.com/login" , "https://cartique-1.onrender.com"],
+ origin:["https://cartiquefrontend.onrender.com" , "https://cartique-1.onrender.com"],
  credentials:true
 }))
 app.get("/", (req, res) => res.send("Backend is running"));
